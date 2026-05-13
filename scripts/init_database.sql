9deffd8412da94784c8272e9389def37ed9e367f
jmalarchik <jmalarchik@gmail.com>
2026-05-13 14:57:34 -0400

    Add warning for dropping DataWarehouse database
    Added a warning comment about dropping the DataWarehouse database.

--- scripts/init_database.sql
+++ scripts/init_database.sql
@@ -1,3 +1,15 @@
+/*
+=========================================
+Create Database and Schema
+=========================================
+
+WARNING:
+  Running this script will drop the entire 'DataWarehouse' database if it exists.
+  All data in the database will be permanently deleted. Proced with caution and
+  ensure you have proper backups before running this script.
+*/
+
+
 USE master;
 GO
 
